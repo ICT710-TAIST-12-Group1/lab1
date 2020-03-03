c257fdebb2ace20c96b24005ba5eabd56d2415e3
tritle129 <tritle129@gmail.com>
2020-03-03 19:11:34 +0700

    push-temp/

---
+++ push-temp/http_main.cpp
@@ -0,0 +1,142 @@
+#include <mbed.h>
+#include "https_request.h"
+
+#include "stm32l475e_iot01_tsensor.h"
+#include "stm32l475e_iot01_hsensor.h"
+    
+DigitalOut led(LED1);
+InterruptIn button(USER_BUTTON);
+Thread t;
+EventQueue queue(5 * EVENTS_EVENT_SIZE);
+Serial pc(USBTX, USBRX);
+WiFiInterface *wifi;
+
+int sensor_value_temp = 0;
+int sensor_value_hum = 0;
+
+const char SSL_CA_PEM[] = "-----BEGIN CERTIFICATE-----\n"
+"MIIDxTCCAq2gAwIBAgIQAqxcJmoLQJuPC3nyrkYldzANBgkqhkiG9w0BAQUFADBs\n"
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+"d3cuZGlnaWNlcnQuY29tMSswKQYDVQQDEyJEaWdpQ2VydCBIaWdoIEFzc3VyYW5j\n"
+"ZSBFViBSb290IENBMB4XDTA2MTExMDAwMDAwMFoXDTMxMTExMDAwMDAwMFowbDEL\n"
+"MAkGA1UEBhMCVVMxFTATBgNVBAoTDERpZ2lDZXJ0IEluYzEZMBcGA1UECxMQd3d3\n"
+"LmRpZ2ljZXJ0LmNvbTErMCkGA1UEAxMiRGlnaUNlcnQgSGlnaCBBc3N1cmFuY2Ug\n"
+"RVYgUm9vdCBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMbM5XPm\n"
+"+9S75S0tMqbf5YE/yc0lSbZxKsPVlDRnogocsF9ppkCxxLeyj9CYpKlBWTrT3JTW\n"
+"PNt0OKRKzE0lgvdKpVMSOO7zSW1xkX5jtqumX8OkhPhPYlG++MXs2ziS4wblCJEM\n"
+"xChBVfvLWokVfnHoNb9Ncgk9vjo4UFt3MRuNs8ckRZqnrG0AFFoEt7oT61EKmEFB\n"
+"Ik5lYYeBQVCmeVyJ3hlKV9Uu5l0cUyx+mM0aBhakaHPQNAQTXKFx01p8VdteZOE3\n"
+"hzBWBOURtCmAEvF5OYiiAhF8J2a3iLd48soKqDirCmTCv2ZdlYTBoSUeh10aUAsg\n"
+"EsxBu24LUTi4S8sCAwEAAaNjMGEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQF\n"
+"MAMBAf8wHQYDVR0OBBYEFLE+w2kD+L9HAdSYJhoIAu9jZCvDMB8GA1UdIwQYMBaA\n"
+"FLE+w2kD+L9HAdSYJhoIAu9jZCvDMA0GCSqGSIb3DQEBBQUAA4IBAQAcGgaX3Nec\n"
+"nzyIZgYIVyHbIUf4KmeqvxgydkAQV8GK83rZEWWONfqe/EW1ntlMMUu4kehDLI6z\n"
+"eM7b41N5cdblIZQB2lWHmiRk9opmzN6cN82oNLFpmyPInngiK3BD41VHMWEZ71jF\n"
+"hS9OMPagMRYjyOfiZRYzy78aG6A9+MpeizGLYAiJLQwGXFK3xPkKmNEVX58Svnw2\n"
+"Yzi9RKR/5CYrCsSXaQ3pjOLAEFe4yHYSkVXySGnYvCoCWw9E1CAx2/S6cCZdkGCe\n"
+"vEsXCS+0yx5DaMkHJ8HSXPfqIbloEpw8nL+e/IBcm2PN7EeqJSdnoDfzAIJ9VNep\n"
+"+OkuE6N36B9K\n"
+"-----END CERTIFICATE-----\n";
+
+
+void read_temp(){
+    pc.printf("Start sensor init\n");
+ 
+    BSP_TSENSOR_Init();
+    BSP_HSENSOR_Init();
+
+         sensor_value_temp = BSP_TSENSOR_ReadTemp();
+        //  pc.printf("\nTEMPERATURE = %d degC\n", sensor_value_temp);
+         sensor_value_hum = BSP_HSENSOR_ReadHumidity();
+         
+
+}
+
+const char *sec2str(nsapi_security_t sec)
+{
+    switch (sec) {
+        case NSAPI_SECURITY_NONE:
+            return "None";
+        case NSAPI_SECURITY_WEP:
+            return "WEP";
+        case NSAPI_SECURITY_WPA:
+            return "WPA";
+        case NSAPI_SECURITY_WPA2:
+            return "WPA2";
+        case NSAPI_SECURITY_WPA_WPA2:
+            return "WPA/WPA2";
+        case NSAPI_SECURITY_UNKNOWN:
+        default:
+            return "Unknown";
+    }
+}
+
+
+
+
+
+void pressed_handler() {
+    
+    
+
+    pc.printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
+    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+        pc.printf("\nConnection error: %d\n", ret);
+        return;
+    }
+
+    pc.printf("Success\n\n");
+    printf("MAC: %s\n", wifi->get_mac_address());
+    printf("IP: %s\n", wifi->get_ip_address());
+    printf("Netmask: %s\n", wifi->get_netmask());
+    printf("Gateway: %s\n", wifi->get_gateway());
+    printf("RSSI: %d\n\n", wifi->get_rssi());  
+
+    // wifi->disconnect();
+    pc.printf("\nDone\n");    
+}
+
+int main() {
+    wifi = WiFiInterface::get_default_instance();
+    if (!wifi) {
+        printf("ERROR: No WiFiInterface found.\n");
+        return -1;
+    }
+pressed_handler();
+    // t.start(callback(&queue, &EventQueue::dispatch_forever));
+    // button.fall(queue.event(pressed_handler));
+    pc.printf("Starting\n");
+    while(1) {
+        
+        // led = !led;        
+        read_temp();
+{
+        HttpsRequest* request = new HttpsRequest(wifi, SSL_CA_PEM, HTTP_POST, "https://testysoftware.herokuapp.com/temp");
+        request->set_header("Content-Type", "application/json");
+        char body[100];
+        sprintf(body, "{\"temp\": %d}", sensor_value_temp);
+        HttpResponse* response = request->send(body, strlen(body));
+        printf("\n----- HTTPS POST response -----\n");
+        printf("status is %d - %s\n", response->get_status_code(), response->get_status_message());
+        
+        delete request;
+        pc.printf("\nTEMPERATURE = %d degC\n", sensor_value_temp);
+    }
+{
+        HttpsRequest* request = new HttpsRequest(wifi, SSL_CA_PEM, HTTP_POST, "https://testysoftware.herokuapp.com/hum");
+        request->set_header("Content-Type", "application/json");
+        char body[100];
+        sprintf(body, "{\"hum\": %d}", sensor_value_hum);
+        HttpResponse* response = request->send(body, strlen(body));
+        printf("\n----- HTTPS POST response -----\n");
+        printf("status is %d - %s\n", response->get_status_code(), response->get_status_message());
+        
+        delete request;
+        pc.printf("HUMIDITY    = %d \n", sensor_value_hum);
+    }
+
+    ThisThread::sleep_for(60000);
+    }
+
+}
